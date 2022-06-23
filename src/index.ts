d84ef3b5406e49367c21100dc7b1b6e0c2c7af88
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2022-06-23 15:24:55 +0600

    Authors and books removed.

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { ApolloServer } from 'apollo-server';
-import { authorsResolvers } from './resolvers/authors';
 
 /*Local dependencies */
-import { booksResolvers } from './resolvers/books';
 import { usersResolvers } from './resolvers/users';
 
 const server = new ApolloServer({
   typeDefs: loadSchemaSync('src/schemas/**/*.graphql', {
     loaders: [new GraphQLFileLoader()],
   }),
-  resolvers: [booksResolvers, authorsResolvers, usersResolvers],
+  resolvers: [usersResolvers],
 });
 
 server.listen().then(({ url }: any) => {
